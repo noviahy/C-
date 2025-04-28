be055191070e2f7f4deb1fb95184aba905caae3e
noviahy <noviahyunji@gmail.com>
2025-04-28 23:05:26 +0900

    25\04\28

---
+++ runProgram/Variable.cs
@@ -0,0 +1,12 @@
+// 25.04.28-73p
+
+using System;
+
+class Literal{
+    static void Main(){
+        Console.WriteLine(1234);
+        Console.WriteLine(3.14F);
+        Console.WriteLine('A');
+        Console.WriteLine("HELLO");
+    }
+}
